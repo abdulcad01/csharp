a740b7a0a9dc6b3a10e112b9ef94b143ab9f7f64
abdulcad01 <abdulrahmancad01>
2024-02-26 18:08:52 +0530

    added c# files

---
+++ Day 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+class Thagappa
+{
+    public string dad = "Abdullah";
+    public void Dada()
+    {
+        Console.WriteLine("Bawa");
+    }
+}
+
+//Second class
+
+class Magan : Thagappa
+{
+    public string son = "Abbu";
+    public void Beta()
+    {
+        Console.WriteLine("Abbu");
+    }
+  
+}
+class Family
+{
+    static void Main(string[] args)
+    {
+    Magan paiyan = new Magan();
+    paiyan.Dada();
+    paiyan.Beta();
+  
+    }
+}
